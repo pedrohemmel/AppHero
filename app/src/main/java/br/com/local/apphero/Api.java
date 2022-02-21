1e360a20c2ca58f6897b29554fbae6ef704e3875
pedrohemmel <phenrique_h2016@hotmail.com>
2022-02-21 11:52:40 -0300

    Passando variáveis API

---
+++ app/src/main/java/br/com/local/apphero/Api.java
@@ -0,0 +1,10 @@
+package br.com.local.apphero;
+
+public class Api {
+    private static final String ROOT_URL = "";
+
+    public static final String URL_CREATE_HERO = ROOT_URL + "createhero";
+    public static final String URL_READ_HEROES = ROOT_URL + "getheroes";
+    public static final String URL_UPDATE_HERO = ROOT_URL + "updatehero";
+    public static final String URL_DELETE_HERO = ROOT_URL + "deletehero&id=";
+}
