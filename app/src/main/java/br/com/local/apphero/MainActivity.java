ca8b6729e4f3523baa0aea43d29252b0174fe3be
pedrohemmel <phenrique_h2016@hotmail.com>
2022-02-23 11:51:26 -0300

    CONECTANDO COM BANCO DE DADOS (INCOMPLETO)

--- app/src/main/java/br/com/local/apphero/MainActivity.java
+++ app/src/main/java/br/com/local/apphero/MainActivity.java
@@ -1,14 +1,263 @@
 package br.com.local.apphero;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static android.view.View.GONE;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CODE_GET_REQUEST = 1024;
+    private static final int CODE_POST_REQUEST = 1025;
+
+    EditText editTextHeroId, editTextName, editTextRealName;
+    RatingBar ratingBar;
+    Spinner spinnerTeam;
+    ProgressBar progressBar;
+    ListView listView;
+    Button buttonAddUpdate;
+
+    List<Hero> heroList;
+
+    boolean isUpdating = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextHeroId = findViewById(R.id.editTextHeroId);
+        editTextName = findViewById(R.id.editTextName);
+        editTextRealName = findViewById(R.id.editTextRealName);
+        ratingBar = findViewById(R.id.ratingBar);
+        spinnerTeam = findViewById(R.id.spinnerTeamAffiliation);
+
+        buttonAddUpdate = findViewById(R.id.buttonAddUpdate);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        listView = findViewById(R.id.listVewHeroes);
+
+        heroList = new ArrayList<>();
+
+        buttonAddUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isUpdating) {
+                    updateHero();
+                } else {
+                    createHero();
+                }
+            }
+        });
+
+        readHero();
     }
+
+    private void createHero() {
+        String name = editTextName.getText().toString().trim();
+        String realName = editTextRealName.getText().toString().trim();
+
+        int rating = (int) ratingBar.getRating();
+
+        String team = spinnerTeam.getSelectedItem().toString();
+
+        if(TextUtils.isEmpty(name)) {
+            editTextName.setError("Por favor entre com o nome.");
+            editTextName.requestFocus();
+            return;
+        }
+
+        if(TextUtils.isEmpty(realName)) {
+            editTextRealName.setError("Por favor entre com o nome real.");
+            editTextRealName.requestFocus();
+            return;
+        }
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("realName", realName);
+        params.put("rating", String.valueOf(rating));
+        params.put("teamAffiliation", team);
+
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_CREATE_HERO, params, CODE_POST_REQUEST);
+        request.execute();
+    }
+
+    private void readHeroe() {
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_READ_HEROES, null, CODE_GET_REQUEST);
+        request.execute();
+    }
+
+    private void updateHero() {
+        String id = editTextHeroId.getText().toString();
+        String name = editTextName.getText().toString().trim();
+        String realName = editTextRealName.getText().toString().trim();
+
+        int rating = (int) ratingBar.getRating();
+
+        String team = spinnerTeam.getSelectedItem().toString();
+
+        if(TextUtils.isEmpty(name)) {
+            editTextName.setError("Por favor entre com o nome");
+            editTextName.requestFocus();
+            return;
+        }
+
+        if(TextUtils.isEmpty(realName)) {
+            editTextRealName.setError("Por favor entre com o nome");
+            editTextRealName.requestFocus();
+            return;
+        }
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("id", id);
+        params.put("name", name);
+        params.put("realName", realName);
+        params.put("rating", String.valueOf(rating));
+        params.put("teamAffiliation", team);
+
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_UPDATE_HERO, params, CODE_POST_REQUEST);
+        request.execute();
+
+        buttonAddUpdate.setText("Adicionar");
+
+        editTextName.setText("");
+        editTextRealName.setText("");
+        ratingBar.setRating(0);
+        spinnerTeam.setSelection(0);
+
+        isUpdating = false;
+    }
+
+    private void deleteHero(int id) {
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_DELETE_HERO + id, null, CODE_GET_REQUEST);
+        request.execute();
+    }
+
+    private void refreshHeroList(JSONArray heroes) throws JSONException {
+        heroList.clear();
+
+        for(int i = 0; i < heroes.length(); i++) {
+            JSONObject obj = heroes.getJSONObject(i);
+
+            heroList.add(new Hero(
+                    obj.getInt("id"),
+                    obj.getString("name"),
+                    obj.getString("realName"),
+                    obj.getInt("rating"),
+                    obj.getString("teamAffiliation")
+            ));
+        }
+        HeroAdater adapter = new HeroAdapter(heroList);
+        listView.setAdapter(adapter);
+    }
+
+    private class PerformNetworkRequest extends AsyncTask<Void, Void, String> {
+
+        String url:
+        HashMap<String, String> params;
+        int requestCode;
+
+        PerformNetworkRequest(String url, HashMap<String, String> params, int requestCode) {
+            this.url = url;
+            this.params = params;
+            this.requestCode = requestCode;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressBar.setVisibility(GONE);
+            try{
+                JSONObject object = new JSONObject(s);
+                if(!object.getBoolean("error")) {
+                    Toast.makeText(getApplicationContext(), object.getString("message"),
+                            toast.LENGTH_SHORT).show();
+                    refreshHeroList(object.getJSONArray("heroes"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            RequestHandler requestHandler = new RequestHandler();
+
+            if(requestCode == CODE_POST_REQUEST)
+                return requestHandler.sendPostRequest(url, params);
+
+            if(requestCode == CODE_GET_REQUEST)
+                return requestHandler.sendGetRequest(url);
+            return null;
+        }
+    }
+
+    class HeroAdapter extends ArrayAdapter<Hero> {
+        List<Hero> heroList;
+
+        public HeroAdapter(List<Hero> heroList) {
+            super(MainActivity.this, R.layout.modelo_hero, heroList);
+            this.heroList = heroList;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater inflater = getLayoutInflater();
+            View listViewItem = inflater.inflate(R.layout.modelo_hero, null, true);
+
+            TextView textViewName = listViewItem.findViewById(R.id.textViewName);
+
+            TextView textViewUpdate = listViewItem.findViewById(R.id.textViewUpdate);
+            TextView textViewDelete = listViewItem.findViewById(R.id.textViewDelete);
+
+            final Hero hero = heroList.get(position);
+
+            textViewName.setText(hero.getName());
+            textViewUpdate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    isUpdating = true;
+                    //CONTINUAR NA PROXIMA AULA
+                }
+            });
+
+            return super.getView(position, convertView, parent);
+        }
+    }
+
 }
